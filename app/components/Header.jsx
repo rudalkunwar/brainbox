11f8626619186488caefc36d962f9db11d59e385
rudalkunwar <ruzalkunwar@gmail.com>
2024-10-03 19:17:59 +0545

    login and register pages

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,11 +5,14 @@ function Header() {
         <div className='continer mx-12'>
             <nav className='flex justify-between  gap-5 py-4'>
                 <div>
-                    <span>BrainBox</span>
+                    <span><Link href={'/'}> BrainBox</Link></span>
                 </div>
                 <ul className='flex justify-center gap-4'>
                     <li className='hover:text-blue-500 flex justify-center items-center gap-1'>
                         <Link href={'/'}> Home</Link>
+                    </li>
+                    <li>
+                        <Link href="/quiz">Quizzes</Link>
                     </li>
                     <li className='hover:text-blue-500'>
                         <Link href={'about'}> About</Link>
